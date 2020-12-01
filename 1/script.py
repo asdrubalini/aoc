64596fa12838ed515b56569d6d6c734d2b175931
Giovanni <indirizzoemail@mail.com>
2020-12-02 00:12:59 +0100

    solve part 2 of #1

--- 1/script.py
+++ 1/script.py
@@ -6,5 +6,6 @@ with open("input.txt", "r") as f:
 
 for n1 in numbers:
     for n2 in numbers:
-        if (n1 + n2) == 2020:
-            print(n1 * n2)
+        for n3 in numbers:
+            if (n1 + n2 + n3) == 2020:
+                print(n1 * n2 * n3)
