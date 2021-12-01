5e6d9933656690cab867794c43c25dc043df320d
Asdrubalini <indirizzoemail@mail.com>
2021-12-01 11:47:00 +0100

    define structs for all days

--- src/aoc/solutions.rs
+++ src/aoc/solutions.rs
@@ -3,6 +3,30 @@ use itertools::Itertools;
 use super::Solution;
 
 pub struct DayOne;
+pub struct DayTwo;
+pub struct DayThree;
+pub struct DayFour;
+pub struct DayFive;
+pub struct DaySix;
+pub struct DaySeven;
+pub struct DayEight;
+pub struct DayNine;
+pub struct DayTen;
+pub struct DayEleven;
+pub struct DayTwelve;
+pub struct DayThirteen;
+pub struct DayFourteen;
+pub struct DayFifteen;
+pub struct DaySixteen;
+pub struct DaySeventeen;
+pub struct DayEighteen;
+pub struct DayNineteen;
+pub struct DayTwenty;
+pub struct DayTwentyOne;
+pub struct DayTwentyTwo;
+pub struct DayTwentyThree;
+pub struct DayTwentyFour;
+pub struct DayTwentyFive;
 
 impl Solution for DayOne {
     type Output = usize;
