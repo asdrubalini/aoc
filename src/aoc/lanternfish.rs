57914934e45e8400244751939d17228cfa61945e
Asdrubalini <indirizzoemail@mail.com>
2021-12-06 16:35:30 +0100

    complete day 6 part 2

--- src/aoc/lanternfish.rs
+++ src/aoc/lanternfish.rs
@@ -1,71 +1,61 @@
+use std::mem::replace;
+
 use super::Solution;
 
-struct Lanternfish {
-    timer: u8,
+#[derive(Debug)]
+struct LanternfishGroup {
+    counter: u32,
+    fishes: Vec<u64>,
 }
 
-impl Iterator for Lanternfish {
-    type Item = Option<Self>;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        match self.timer {
-            0 => {
-                self.timer = 6;
-                Some(Some(Self::new()))
-            }
-
-            _ => {
-                self.timer -= 1;
-                Some(None)
-            }
-        }
-    }
-}
-
-impl Lanternfish {
-    fn new() -> Self {
-        Self { timer: 8 }
-    }
-
-    fn with_timer(timer: u8) -> Self {
-        Self { timer }
-    }
-}
-
-struct LanternfishSchool {
-    fishes: Vec<Lanternfish>,
-}
-
-impl LanternfishSchool {
+impl LanternfishGroup {
     fn new(timers: &[u8]) -> Self {
-        Self {
-            fishes: timers
-                .iter()
-                .map(|timer| Lanternfish::with_timer(*timer))
-                .collect(),
+        let mut fishes = vec![0; 9];
+
+        for timer in timers {
+            let counter = fishes.get_mut(*timer as usize).unwrap();
+            *counter += 1;
         }
+
+        Self { counter: 0, fishes }
     }
 
     fn advance_all(&mut self) {
-        let new_fishes = self
-            .fishes
-            .iter_mut()
-            .map(|fish| fish.next().unwrap())
-            .filter_map(|fish| fish)
-            .collect::<Vec<_>>();
+        self.counter += 1;
+        dbg!(self.counter);
 
-        for fish in new_fishes {
-            self.fishes.push(fish);
+        let fishes = replace(&mut self.fishes, vec![0; 9]);
+
+        for (timer, count) in fishes.into_iter().enumerate() {
+            println!("{} -> {}", timer, count);
+
+            match timer {
+                0 => {
+                    let counter = self.fishes.get_mut(6).unwrap();
+                    *counter += count;
+
+                    let counter = self.fishes.get_mut(8).unwrap();
+                    *counter += count;
+                }
+
+                n => {
+                    let counter = self.fishes.get_mut(n - 1).unwrap();
+                    *counter += count;
+                }
+            }
         }
+
+        // dbg!(&self.fishes);
+        println!();
     }
 }
 
-impl Iterator for LanternfishSchool {
-    type Item = usize;
+impl Iterator for LanternfishGroup {
+    type Item = u64;
 
     fn next(&mut self) -> Option<Self::Item> {
         self.advance_all();
-        Some(self.fishes.len())
+        Some(self.fishes.iter().map(|n| *n).sum())
     }
 }
 
@@ -84,7 +74,7 @@ impl DaySix {
 }
 
 impl Solution for DaySix {
-    type Output = u32;
+    type Output = u64;
 
     fn input() -> &'static str {
         include_str!("./inputs/6.txt")
@@ -92,16 +82,19 @@ impl Solution for DaySix {
 
     fn solve_first(input: &str) -> Self::Output {
         let input = Self::parse_input(input);
-        let mut school = LanternfishSchool::new(&input);
+        let mut group = LanternfishGroup::new(&input);
 
-        school.nth(80 - 1).unwrap() as u32
+        group.nth(80 - 1).unwrap()
     }
 
     fn solve_second(input: &str) -> Self::Output {
-        0
+        let input = Self::parse_input(input);
+        let mut group = LanternfishGroup::new(&input);
+
+        group.nth(256 - 1).unwrap()
     }
 
     fn expected_solutions() -> (Self::Output, Self::Output) {
-        (351188, 0)
+        (351188, 1595779846729)
     }
 }
