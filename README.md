8340d85ff91e43585a6db449489d474687d2baca
Asdrubalini <indirizzoemail@mail.com>
2021-12-10 23:08:20 +0100

    parse parse parse

--- README.md
+++ README.md
@@ -4,15 +4,15 @@ Run tests: `cargo test`
 # Benchmarks:
 | Day | First part | Second part |
 | --- | --- | --- |
-| DayOne | 39.871µs | 38.879µs |
-| DayTwo | 85.145µs | 91.387µs |
-| DayThree | 231.622µs | 286.647µs |
-| DayFour | 3.68647ms | 3.70291ms |
-| DayFive | 1.482132ms | 1.791384ms |
-| DaySix | 7.249µs | 11.586µs |
-| DaySeven | 490.197µs | 5.085715ms |
-| DayEight | 731.759µs | 884.284µs |
-| DayNine | 329.667µs | 2.496278ms |
+| DayOne | 84.028µs | 124.051µs |
+| DayTwo | 212.401µs | 203.745µs |
+| DayThree | 195.763µs | 243.666µs |
+| DayFour | 3.657174ms | 3.697738ms |
+| DayFive | 1.498052ms | 1.848839ms |
+| DaySix | 7.281µs | 11.728µs |
+| DaySeven | 487.13µs | 4.975557ms |
+| DayEight | 725.099µs | 888.149µs |
+| DayNine | 328.559µs | 2.322564ms |
 
 
-Total: `21.473182ms`
+Total: `21.511524ms`
--- src/aoc/day_ten.rs
+++ src/aoc/day_ten.rs
@@ -20,29 +20,61 @@ struct ChunksEntry {
     error_score: u32,
 }
 
+#[derive(Debug)]
+enum ErrorKind {
+    CloseBracketNotFound,
+    MismatchedCloseBracket,
+}
+
 impl ChunksEntry {
-    fn from_str(input: &str) -> Self {
+    fn from_str(input: &str) -> Result<Self, ErrorKind> {
         let tokens = Token::lex(input);
 
-        let node = Self::parse_tokens(&tokens, 0);
+        let mut current_pos = 0;
+        let mut nodes = vec![];
 
-        Self {
-            brackets: vec![node.1.unwrap()],
-            error_score: 0,
+        loop {
+            let (next_pos, node) = Self::parse_tokens(&tokens, current_pos + 1)?;
+            current_pos = next_pos;
+
+            match node {
+                Some(node) => nodes.push(node),
+                None => break,
+            }
         }
+
+        Ok(Self {
+            brackets: nodes,
+            error_score: 0,
+        })
     }
 
-    fn parse_tokens(tokens: &[Token], start_pos: usize) -> (usize, Option<ChunkNode>) {
-        println!("{}", start_pos);
-
+    fn parse_tokens(
+        tokens: &[Token],
+        start_pos: usize,
+    ) -> Result<(usize, Option<ChunkNode>), ErrorKind> {
         if start_pos + 1 > tokens.len() {
-            return (start_pos, None);
+            return Ok((start_pos, None));
         }
 
         let start_token = tokens[start_pos];
 
         if start_token.is_close() {
-            return (start_pos, None);
+            return Ok((start_pos, None));
+        }
+
+        let close_token = start_token.to_close();
+        let close_tokens = tokens
+            .iter()
+            .skip(start_pos)
+            .filter(|token| token.is_close())
+            .filter(|token| **token == close_token)
+            .collect::<Vec<_>>();
+
+        if close_tokens.len() == 0 {
+            return Err(ErrorKind::CloseBracketNotFound);
+        } else if close_tokens.len() % 2 == 0 {
+            return Err(ErrorKind::MismatchedCloseBracket);
         }
 
         let mut current_pos = start_pos;
@@ -52,7 +84,7 @@ impl ChunksEntry {
         };
 
         loop {
-            let (next_pos, node) = Self::parse_tokens(tokens, current_pos + 1);
+            let (next_pos, node) = Self::parse_tokens(tokens, current_pos + 1)?;
             current_pos = next_pos;
 
             match node {
@@ -61,7 +93,7 @@ impl ChunksEntry {
             }
         }
 
-        (current_pos, Some(current_node))
+        Ok((current_pos, Some(current_node)))
     }
 }
 
@@ -106,6 +138,16 @@ impl Token {
         }
     }
 
+    fn to_close(self) -> Self {
+        match self {
+            Token::RoundOpen => Token::RoundClose,
+            Token::SquareOpen => Token::SquareClose,
+            Token::CurlyOpen => Token::CurlyClose,
+            Token::AngleOpen => Token::AngleClose,
+            _ => panic!("Cannot convert open Token to close"),
+        }
+    }
+
     fn kind(self) -> BracketKind {
         match self {
             Token::RoundOpen | Token::RoundClose => BracketKind::Round,
@@ -119,7 +161,7 @@ impl Token {
 pub struct DayTen;
 
 impl DayTen {
-    fn parse_input(input: &str) -> Vec<ChunksEntry> {
+    fn parse_input(input: &str) -> Vec<Result<ChunksEntry, ErrorKind>> {
         input
             .lines()
             .filter(|line| !line.is_empty())
@@ -132,15 +174,20 @@ impl Solution for DayTen {
     type Output = u64;
 
     fn input() -> &'static str {
-        // include_str!("./inputs/example.txt")
-        // "[<>({}){}[([])<>]]"
-        "([][])"
+        include_str!("./inputs/example.txt")
     }
 
     fn solve_first(input: &str) -> Self::Output {
         let input = Self::parse_input(input);
 
-        println!("{:?}", input.iter().next().unwrap());
+        for entry in input {
+            match entry {
+                Ok(entry) => println!(" ok"),
+                Err(error) => {
+                    println!(" {:?}", error);
+                }
+            }
+        }
 
         0
     }
