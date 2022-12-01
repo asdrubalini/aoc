a387a9656081a202eec0f6b1e04f6dfe6bcf88ed
Asdrubalini <asdrubalini@mail.com>
2022-12-01 10:54:39 +0100

    fix all files

---
+++ src/aoc/days/three.rs
@@ -0,0 +1,28 @@
+use crate::aoc::Solution;
+
+pub struct Three;
+
+impl Solution for Three {
+    type Output = usize;
+    type Parsed = Vec<u32>;
+
+    fn input() -> &'static str {
+        ""
+    }
+
+    fn parse_input(_input: &'static str) -> Self::Parsed {
+        vec![]
+    }
+
+    fn solve_first(_parsed: &Self::Parsed) -> Self::Output {
+        0
+    }
+
+    fn solve_second(_parsed: &Self::Parsed) -> Self::Output {
+        0
+    }
+
+    fn expected_solutions() -> (Self::Output, Self::Output) {
+        (0, 0)
+    }
+}
