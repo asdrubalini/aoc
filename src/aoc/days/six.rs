5746bf543837a6808b04a64efce312b377b9aeb5
Asdrubalini <asdrubalini@mail.com>
2022-12-06 12:28:24 +0100

    solve day six part one

--- src/aoc/days/six.rs
+++ src/aoc/days/six.rs
@@ -1,21 +1,29 @@
+use itertools::Itertools;
+
 use crate::aoc::Solution;
 
 pub struct Six;
 
 impl Solution for Six {
     type Output = u32;
-    type Parsed = Vec<u32>;
+    type Parsed = String;
 
     fn input() -> &'static str {
-        ""
+        include_str!("../inputs/6.txt")
     }
 
-    fn parse_input(_input: &'static str) -> Self::Parsed {
-        vec![]
+    fn parse_input(input: &'static str) -> Self::Parsed {
+        input.to_string()
     }
 
-    fn solve_first(_parsed: &Self::Parsed) -> Self::Output {
-        0
+    fn solve_first(parsed: &Self::Parsed) -> Self::Output {
+        for (pos, (c1, c2, c3, c4)) in parsed.chars().tuple_windows().enumerate() {
+            if vec![c1, c2, c3, c4].iter().unique().count() == 4 {
+                return pos as u32 + 4;
+            }
+        }
+
+        panic!("cannot find")
     }
 
     fn solve_second(_parsed: &Self::Parsed) -> Self::Output {
