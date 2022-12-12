942d930849c4de29e4f44b261f9eb523b89db556
Asdrubalini <asdrubalini@mail.com>
2022-12-12 01:16:38 +0100

    solve day eleven part two

--- src/aoc/days/eleven.rs
+++ src/aoc/days/eleven.rs
@@ -33,39 +33,9 @@ impl From<&str> for Operation {
 impl Operation {
     fn apply(&self, mut worry_level: WorryLevel) -> WorryLevel {
         match self {
-            Operation::Sum(a) => {
-                worry_level.worry_level += *a as u64;
-            }
-            Operation::Product(a) => {
-                worry_level.worry_level *= *a as u64;
-            }
-            Operation::Square => {
-                worry_level.worry_level *= worry_level.worry_level;
-            }
-        }
-
-        worry_level
-    }
-
-    fn apply_soft(&self, mut worry_level: WorryLevel) -> WorryLevel {
-        match self {
-            Operation::Sum(a) => {
-                let sum = worry_level.worry_level + *a as u64;
-                worry_level.worry_level = sum;
-                worry_level.divisible_by = WorryLevel::find_dividends(sum);
-            }
-            Operation::Product(a) => match *a {
-                3 => {
-                    worry_level.set_divisible(3);
-                }
-                19 => {
-                    worry_level.set_divisible(19);
-                }
-                _ => panic!("wtf"),
-            },
-            Operation::Square => {
-                //*worry_level.divisible_by.get_mut(&2).unwrap() = true;
-            }
+            Operation::Sum(a) => worry_level.sum(*a as u32),
+            Operation::Product(a) => worry_level.multiply(*a as u32),
+            Operation::Square => worry_level.square(),
         }
 
         worry_level
@@ -74,41 +44,51 @@ impl Operation {
 
 #[derive(Debug, Clone)]
 pub struct WorryLevel {
-    worry_level: u64,
-    divisible_by: HashMap<u8, bool>,
+    modulos: HashMap<u8, u32>,
 }
 
 impl WorryLevel {
-    fn is_divisible_by(&self, dividend: u8) -> bool {
-        *self.divisible_by.get(&dividend).unwrap()
-    }
-
-    fn find_dividends(worry_level: u64) -> HashMap<u8, bool> {
-        [2, 3, 5, 7, 11, 13, 17, 19, 23]
-            .map(|dividend| (dividend as u8, worry_level % dividend == 0))
-            .into()
-    }
-
-    fn set_divisible(&mut self, dividend: u8) {
-        let ciao = self.divisible_by.get_mut(&dividend).unwrap();
-        *ciao = true;
-    }
-}
-
-impl From<u64> for WorryLevel {
-    fn from(worry_level: u64) -> Self {
-        let divisible_by = Self::find_dividends(worry_level);
-
-        WorryLevel {
-            worry_level,
-            divisible_by,
+    fn sum(&mut self, other: u32) {
+        for (modulo, value) in self.modulos.iter_mut() {
+            *value += other;
+            *value %= *modulo as u32;
         }
     }
+
+    fn multiply(&mut self, other: u32) {
+        for (modulo, value) in self.modulos.iter_mut() {
+            *value *= other;
+            *value %= *modulo as u32;
+        }
+    }
+
+    fn square(&mut self) {
+        for (modulo, value) in self.modulos.iter_mut() {
+            *value *= *value;
+            *value %= *modulo as u32;
+        }
+    }
+
+    fn divide_floor(&mut self, other: u32) {
+        for (modulo, value) in self.modulos.iter_mut() {
+            *value += other;
+            *value %= *modulo as u32;
+        }
+    }
+
+    fn is_divisible_by(&self, divisor: u8) -> bool {
+        *self.modulos.get(&divisor).unwrap() == 0
+    }
 }
 
-impl Into<u64> for WorryLevel {
-    fn into(self) -> u64 {
-        self.worry_level
+impl From<u32> for WorryLevel {
+    fn from(worry_level: u32) -> Self {
+        let modulos = [2, 3, 5, 7, 11, 13, 17, 19, 23]
+            .into_iter()
+            .map(|modulo| (modulo as u8, worry_level as u32 % modulo))
+            .collect();
+
+        WorryLevel { modulos }
     }
 }
 
@@ -132,7 +112,7 @@ impl From<&str> for Monkey {
         let items_worry_level = {
             let line = lines.next().unwrap().replace("Starting items: ", "");
             line.split(", ")
-                .map(|item| item.parse::<u64>().unwrap())
+                .map(|item| item.parse::<u32>().unwrap())
                 .map(WorryLevel::from)
                 .collect_vec()
         };
@@ -182,34 +162,7 @@ impl Solution for Eleven {
     type Parsed = Vec<Monkey>;
 
     fn input() -> &'static str {
-        "Monkey 0:
-  Starting items: 79, 98
-  Operation: new = old * 19
-  Test: divisible by 23
-    If true: throw to monkey 2
-    If false: throw to monkey 3
-
-Monkey 1:
-  Starting items: 54, 65, 75, 74
-  Operation: new = old + 6
-  Test: divisible by 19
-    If true: throw to monkey 2
-    If false: throw to monkey 0
-
-Monkey 2:
-  Starting items: 79, 60, 97
-  Operation: new = old * old
-  Test: divisible by 13
-    If true: throw to monkey 1
-    If false: throw to monkey 3
-
-Monkey 3:
-  Starting items: 74
-  Operation: new = old + 3
-  Test: divisible by 17
-    If true: throw to monkey 0
-    If false: throw to monkey 1"
-        //include_str!("../inputs/11.txt")
+        include_str!("../inputs/11.txt")
     }
 
     fn parse_input(input: &'static str) -> Self::Parsed {
@@ -237,20 +190,19 @@ Monkey 3:
                 };
 
                 while let Some(worry_level) = monkey.items_worry_level.pop_front() {
-                    let worry_level = monkey.operation.apply(worry_level).worry_level as f64;
-                    let worry_level = (worry_level / 3.0).floor();
+                    let mut worry_level = monkey.operation.apply(worry_level);
+                    worry_level.divide_floor(3);
 
-                    let monkey_id = if worry_level % monkey.test_divisible_by as f64 == 0.0 {
+                    let monkey_id = if worry_level.is_divisible_by(monkey.test_divisible_by) {
                         monkey.condition_true_monkey
                     } else {
                         monkey.condition_false_monkey
                     };
 
                     let destination_monkey = monkeys.get_mut(monkey_id).unwrap();
-
                     destination_monkey
                         .items_worry_level
-                        .push_back((worry_level as u64).into());
+                        .push_back(worry_level.into());
                 }
             }
         }
@@ -281,16 +233,15 @@ Monkey 3:
                 };
 
                 while let Some(worry_level) = monkey.items_worry_level.pop_front() {
-                    let worry_level = monkey.operation.apply_soft(worry_level);
+                    let worry_level = monkey.operation.apply(worry_level);
 
-                    let monkey_id = if worry_level.is_divisible_by(monkey.test_divisible_by as u8) {
+                    let monkey_id = if worry_level.is_divisible_by(monkey.test_divisible_by) {
                         monkey.condition_true_monkey
                     } else {
                         monkey.condition_false_monkey
                     };
 
                     let destination_monkey = monkeys.get_mut(monkey_id).unwrap();
-
                     destination_monkey.items_worry_level.push_back(worry_level);
                 }
             }
