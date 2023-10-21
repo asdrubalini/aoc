23fcf5f0fece1e2982a5fe229659f15ac18e3408
Asdrubalini <asdrubalini@mail.com>
2023-10-22 01:24:23 +0200

    day eight part two

--- src/aoc/days/eight.rs
+++ src/aoc/days/eight.rs
@@ -1,13 +1,45 @@
+use std::collections::VecDeque;
+
 use itertools::Itertools;
 
 use crate::aoc::Solution;
 
-trait ParsedLen {
-    fn parsed_len(&self) -> usize;
+trait AocEncoded {
+    fn escape(s: &str) -> String;
+    fn len_in_memory(&self) -> usize;
 }
 
-impl ParsedLen for String {
-    fn parsed_len(&self) -> usize {
+impl AocEncoded for String {
+    fn escape(s: &str) -> String {
+        let mut chars = s.chars().into_iter().peekable();
+
+        let mut escaped_chars = VecDeque::new();
+
+        while let Some(chr) = chars.next() {
+            match chr {
+                '\\' => {
+                    escaped_chars.push_back("\\\\".to_string());
+                }
+
+                '\"' => {
+                    escaped_chars.push_back("\\\"".to_string());
+                }
+
+                _ => {
+                    escaped_chars.push_back(chr.to_string());
+                }
+            }
+        }
+
+        // append and prepend the quotation marks
+        escaped_chars.push_front("\"".to_string());
+        escaped_chars.push_back("\"".to_string());
+
+        todo!()
+    }
+
+    /// Encoded string lenght in memory (without escaping)
+    fn len_in_memory(&self) -> usize {
         let mut chars = self.chars().collect_vec();
         assert_eq!(*chars.first().unwrap(), '"');
         assert_eq!(*chars.last().unwrap(), '"');
@@ -26,7 +58,7 @@ impl ParsedLen for String {
                     let to_skip = match *next {
                         '"' | '\\' => 1,
                         'x' => 3,
-                        _ => panic!("lol",),
+                        _ => panic!("lol"),
                     };
 
                     chars.nth(to_skip - 1);
@@ -55,14 +87,20 @@ impl Solution for Eight {
     }
 
     fn solve_first(parsed: &Self::Parsed) -> Self::Output {
-        let total_len = parsed.iter().map(|s| s.len()).sum::<usize>() as u32;
-        let parsed_len = parsed.iter().map(|s| s.parsed_len()).sum::<usize>() as u32;
+        let original_len = parsed.iter().map(|s| s.len()).sum::<usize>() as u32;
+        let in_memory_len = parsed.iter().map(|s| s.len_in_memory()).sum::<usize>() as u32;
 
-        total_len - parsed_len
+        original_len - in_memory_len
     }
 
-    fn solve_second(_parsed: &Self::Parsed) -> Self::Output {
-        0
+    fn solve_second(parsed: &Self::Parsed) -> Self::Output {
+        let original_len = parsed.iter().map(|s| s.len()).sum::<usize>() as u32;
+        let reescaped_len = parsed
+            .iter()
+            .map(|s| String::escape(s).len())
+            .sum::<usize>() as u32;
+
+        reescaped_len - original_len
     }
 
     fn expected_solutions() -> (Self::Output, Self::Output) {
