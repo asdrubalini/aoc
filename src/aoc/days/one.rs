46aac688a87ab2151d563b6daa80243a19dc5b38
Asdrubalini <asdrubalini@mail.com>
2023-12-02 01:24:54 +0100

    solve day one part two

--- src/aoc/days/one.rs
+++ src/aoc/days/one.rs
@@ -1,4 +1,4 @@
-use std::iter::Sum;
+use std::{char::REPLACEMENT_CHARACTER, collections::HashMap, iter::Sum};
 
 use itertools::Itertools;
 use serde_json::map;
@@ -7,46 +7,68 @@ use crate::aoc::Solution;
 
 pub struct One;
 
-#[derive(Debug)]
-pub struct Calibration(u32);
+trait Calibration {
+    fn calibrate_v1(line: &str) -> u32;
+    fn calibrate_v2(line: &str) -> u32;
+}
 
-impl From<&str> for Calibration {
-    fn from(line: &str) -> Self {
+impl Calibration for u32 {
+    fn calibrate_v1(line: &str) -> u32 {
         let digits = line.chars().filter(|c| c.is_digit(10)).collect_vec();
+
+        dbg!(&digits);
 
         let mut s = String::new();
         s.push(*digits.first().unwrap());
         s.push(*digits.last().unwrap());
 
-        Calibration(s.parse().unwrap())
+        s.parse().unwrap()
+    }
+
+    fn calibrate_v2(line: &str) -> u32 {
+        let mut line = line.to_string();
+
+        dbg!(&line);
+
+        let replacements = [
+            "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+        ];
+
+        for (i, r) in replacements.into_iter().enumerate() {
+            let mut replacement = r.to_string();
+            replacement.push_str(&(i + 1).to_string());
+            replacement.push_str(r);
+
+            line = line.replace(r, &replacement);
+        }
+
+        dbg!(&line);
+
+        Self::calibrate_v1(&line)
     }
 }
 
 impl Solution for One {
     type Output = u32;
-    type Parsed = Vec<Calibration>;
+    type Parsed = Vec<&'static str>;
 
     fn input() -> &'static str {
         include_str!("../inputs/1.txt")
     }
 
     fn parse_input(input: &'static str) -> Self::Parsed {
-        input
-            .lines()
-            .into_iter()
-            .map(Calibration::from)
-            .collect_vec()
+        input.lines().into_iter().collect_vec()
     }
 
     fn solve_first(parsed: &Self::Parsed) -> Self::Output {
-        parsed.iter().map(|c| c.0).sum()
+        parsed.into_iter().map(|l| u32::calibrate_v1(*l)).sum()
     }
 
-    fn solve_second(_parsed: &Self::Parsed) -> Self::Output {
-        0
+    fn solve_second(parsed: &Self::Parsed) -> Self::Output {
+        parsed.into_iter().map(|l| u32::calibrate_v2(*l)).sum()
     }
 
     fn expected_solutions() -> (Self::Output, Self::Output) {
-        (0, 0)
+        (55208, 54578)
     }
 }
