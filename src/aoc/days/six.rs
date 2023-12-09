ccfcfca76581280a18092dee8c2eeaac94b1e7da
Asdrubalini <asdrubalini@mail.com>
2023-12-09 13:06:52 +0100

    solve day six part two

--- src/aoc/days/six.rs
+++ src/aoc/days/six.rs
@@ -3,31 +3,31 @@ use itertools::Itertools;
 
 pub struct Six;
 
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy)]
 pub struct Race {
-    time: u32,
-    distance: u32,
+    time: u64,
+    distance: u64,
 }
 
 impl Race {
-    fn can_beat_the_record(&self, hold_time: u32) -> bool {
+    fn can_beat_the_record(&self, hold_time: u64) -> bool {
         let speed = hold_time; // mm / s
         let moving_duration = self.time - hold_time; // s
-        let travelled_distance = speed * moving_duration; // m
+        let travelled_distance = speed * moving_duration; // mm
 
         travelled_distance > self.distance
     }
 
-    fn bruteforce_record_breaking_ways_count(&self) -> u32 {
+    fn bruteforce_record_breaking_ways_count(&self) -> u64 {
         (0..=self.time)
             .into_iter()
             .filter(|hold_time| self.can_beat_the_record(*hold_time))
-            .count() as u32
+            .count() as u64
     }
 }
 
-impl From<(u32, u32)> for Race {
-    fn from(value: (u32, u32)) -> Self {
+impl From<(u64, u64)> for Race {
+    fn from(value: (u64, u64)) -> Self {
         Race {
             time: value.0,
             distance: value.1,
@@ -40,11 +40,32 @@ pub struct SheetOfPaper {
     races: Vec<Race>,
 }
 
+impl SheetOfPaper {
+    fn into_v2(&self) -> Race {
+        let time: String = self
+            .races
+            .iter()
+            .map(|race| race.time.to_string())
+            .collect();
+
+        let distance: String = self
+            .races
+            .iter()
+            .map(|race| race.distance.to_string())
+            .collect();
+
+        Race {
+            time: time.parse().unwrap(),
+            distance: distance.parse().unwrap(),
+        }
+    }
+}
+
 impl From<&str> for SheetOfPaper {
     fn from(input: &str) -> Self {
         let mut lines = input.lines();
 
-        let parse = |a: &str| -> Vec<u32> {
+        let parse = |a: &str| -> Vec<u64> {
             a.split_ascii_whitespace()
                 .skip(1)
                 .map(|s| s.parse().unwrap())
@@ -62,7 +83,7 @@ impl From<&str> for SheetOfPaper {
 }
 
 impl Solution for Six {
-    type Output = u32;
+    type Output = u64;
     type Parsed = SheetOfPaper;
 
     fn input() -> &'static str {
@@ -81,11 +102,12 @@ impl Solution for Six {
             .product()
     }
 
-    fn solve_second(_parsed: &Self::Parsed) -> Self::Output {
-        0
+    fn solve_second(parsed: &Self::Parsed) -> Self::Output {
+        let race = parsed.into_v2();
+        race.bruteforce_record_breaking_ways_count()
     }
 
     fn expected_solutions() -> (Self::Output, Self::Output) {
-        (861300, 0)
+        (861300, 28101347)
     }
 }
